b8e7f9a4074aed71f0b785e3867e91779f853fd0
Ahmed Haitham Rehan <ah607k@gmail.com>
2026-04-21 14:20:42 +0400

    refactor: remove contact form to add contact info cards

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,6 @@ import AboutMeSection from './components/about-me-section';
 import HeroSection from './components/hero-section';
 import ProjectsSection from './components/projects-section';
 import Navbar from './components/navbar';
-import Providers from './components/providers';
 import { StarsBackground } from './components/stars-background';
 import ThemeToggler from './components/theme-toggler';
 import SkillsSection from './components/skills-section';
@@ -10,7 +9,7 @@ import ContactSection from './components/contact-section';
 
 const App = () => {
   return (
-    <Providers>
+    <>
       <header className='py-6 z-20 relative'>
         <div className='container flex items-center justify-between'>
           <img src='/logo.svg' alt='Logo' className='w-16' />
@@ -20,23 +19,25 @@ const App = () => {
       {/* Navbar */}
       <Navbar />
       <StarsBackground className='fixed inset-0 z-0' pointerEvents={false} />
-      <main className='z-20 relative'>
-        {/* Hero Section */}
-        <HeroSection />
+      <div className='relative z-10'>
+        <main>
+          {/* Hero Section */}
+          <HeroSection />
 
-        {/* About Me */}
-        <AboutMeSection />
+          {/* About Me */}
+          <AboutMeSection />
 
-        {/* Projects */}
-        <ProjectsSection />
+          {/* Projects */}
+          <ProjectsSection />
 
-        {/* Skills */}
-        <SkillsSection />
+          {/* Skills */}
+          <SkillsSection />
 
-        {/* Contact Section */}
-        <ContactSection />
-      </main>
-    </Providers>
+          {/* Contact Section */}
+          <ContactSection />
+        </main>
+      </div>
+    </>
   );
 };
 
--- src/lib/constants/index.ts
+++ src/lib/constants/index.ts
@@ -1,3 +1,7 @@
+import GmailLogo from '@/components/icons/gmail-logo';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { HiMapPin } from 'react-icons/hi2';
+
 export const APP_URL = 'https://www.ahmedrehandev.com';
 export const SOCIAL_LINKS = {
   github: 'https://github.com/Ahmed-HA-RE',
@@ -22,4 +26,28 @@ export const TECH_STACK = [
   'PostgreSQL',
   'Prisma',
   'Git',
+];
+
+export const CONTACT_DATA = [
+  {
+    title: 'Email',
+    subtitle: 'Feel free to reach out anytime',
+    buttonText: 'Send Email',
+    value: 'mailto:ah607k@gmail.com',
+    icon: GmailLogo,
+  },
+  {
+    title: 'Phone',
+    subtitle: 'Available for calls and messages',
+    buttonText: '+971 50 126 7437',
+    value: 'tel:+971501267437',
+    icon: FaPhoneAlt,
+  },
+  {
+    title: 'Location',
+    subtitle: 'Based in Abu Dhabi, United Arab Emirates',
+    buttonText: 'View on Map',
+    value: 'https://maps.app.goo.gl/72MVeKgPkavnXvvYA',
+    icon: HiMapPin,
+  },
 ];
